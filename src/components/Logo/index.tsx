eb558cb4fc1aace6b468f6fb1b7056b1e9581be5
mateuskuritza <mateuskuritza@gmail.com>
2023-04-29 15:55:39 -0300

    feat(home-page): create logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+export function Logo() {
+    return (
+        <div className='cursor-default'>
+            <h1 className='text-silver text-xl text-center'>
+                Ponto
+                <br />
+                <span className='font-extrabold'>Ilumeo</span>
+            </h1>
+        </div>
+    );
+}
